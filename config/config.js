2716a6dbb93745e3795363ff7e1cac5c497a2b3a
Shiraz Suleman <shirazsuleman@gmail.com>
2018-04-02 20:08:35 +0200

    Initial commit for MVC authentication.

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// config/config.js
+
+module.exports = {
+    databaseUrl: 'mongodb://localhost/cents-trail',
+    sessionSecret: 'keyboard cat'
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var config = require('./config/config');
+
+mongoose.connect(config.databaseUrl);
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.use(session({ 
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes')(app, passport);
+
+app.listen(port);
+console.log('Listening on port ' + port + '.');
